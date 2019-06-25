2def470c800290e9e025e5554439d678e6e19283
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-25 12:01:51 +0100

    Done

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,1 +1,39 @@
-// Code ControlledInput Component Here
+class Form extends Component {
+  state = {
+    firstName: "John",
+    lastName: "Henry"
+  };
+
+  handleFirstNameChange = event => {
+    this.setState({
+      firstName: event.target.value
+    });
+  };
+
+  handleLastNameChange = event => {
+    this.setState({
+      lastName: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <form>
+        <input
+          type="text"
+          id="firstName"
+          onChange={event => this.handleFirstNameChange(event)}
+          name="firstName"
+          value={this.state.firstName}
+        />
+        <input
+          type="text"
+          id="lastName"
+          onChange={event => this.handleLastNameChange(event)}
+          name="lastName"
+          value={this.state.lastName}
+        />
+      </form>
+    );
+  }
+}
